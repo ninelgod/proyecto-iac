c7d0145f38151a8e15b4d24da1105fcbca2900ce
jamhouse17 <82184135+JoeToribioFlores@users.noreply.github.com>
2025-10-01 08:53:00 -0500

    feat: añadimos 3 outputs y eliminamos el alb_dns_name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "alb_dns_name" {
-  value = aws_lb.app.dns_name
+output "ecr_api_endpoint" {
+  value = aws_vpc_endpoint.ecr_api.dns_entry
+}
+
+output "ecr_dkr_endpoint" {
+  value = aws_vpc_endpoint.ecr_dkr.dns_entry
+}
+
+output "s3_endpoint" {
+  value = aws_vpc_endpoint.s3.dns_entry
 }
