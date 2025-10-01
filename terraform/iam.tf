f22c11becde01e47e1ed8e2d01928a12bf8eac78
jamhouse17 <82184135+JoeToribioFlores@users.noreply.github.com>
2025-10-01 08:51:29 -0500

    refactor: rename ecs_task_execution role resource for clarity

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_role" "ecs_task_execution" {
-  name = "ecs_task_execution_role_reservas"
+  name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
@@ -18,4 +18,9 @@ resource "aws_iam_role" "ecs_task_execution" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_secrets" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
