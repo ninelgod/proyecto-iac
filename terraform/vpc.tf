ed9771c80dc34330aa373b3f268c81634ff52181
jamhouse17 <82184135+JoeToribioFlores@users.noreply.github.com>
2025-10-01 08:59:49 -0500

    feat: agregamos route table publica

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,65 +1,53 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
-data "aws_availability_zones" "available" {}
-
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-
-  tags = {
-    Name = "vet-goicochea-vpc-us-east-2"
-  }
-}
-
-resource "aws_subnet" "public" {
-  count                   = length(data.aws_availability_zones.available.names)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count             = length(data.aws_availability_zones.available.names)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + length(aws_subnet.public))
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "private-subnet-${count.index}"
-  }
+  tags = { Name = "vet-goicochea-vpc-us-east-2" }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+}
 
-  tags = {
-    Name = "vet-goicochea-igw"
-  }
+# Subnets públicas (ALB)
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/20", 4, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  tags = { Name = "public-subnet-${count.index}" }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.64.0/20", 4, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = { Name = "private-subnet-${count.index}" }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
-  tags = {
-    Name = "public-rt"
-  }
 }
 
-resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+resource "aws_route_table_association" "public_assoc" {
+  count          = 2
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "vet-private-rt" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
