06c00fe68fe29998c81dc32a429bb6e52ef95768
jamhouse17 <82184135+JoeToribioFlores@users.noreply.github.com>
2025-10-01 08:44:02 -0500

    feat: cambiar el puerto de escucha a 4000 y agregar verificación de estado

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,26 +1,35 @@
-
 resource "aws_lb" "app" {
-  name               = "reserva-alb"
+  name               = "vet-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.public1.id]
+  subnets            = aws_subnet.public[*].id
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
-resource "aws_lb_target_group" "reserva_tg" {
-  name     = "reserva-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+resource "aws_lb_target_group" "reservas_tg" {
+  name        = "reservas-tg"
+  port        = 4000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path                = "/health"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.reserva_tg.arn
+    target_group_arn = aws_lb_target_group.reservas_tg.arn
   }
 }
